985c5c308c5a85f789ef446163be498a48071b83
sephchan <110228448+sephchan@users.noreply.github.com>
2025-03-30 02:32:32 +0800

    Footer update

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+// function toggleMenu() {
+//   const menu = document.querySelector(".menu-links");
+//   const icon = document.querySelector(".hamburger-icon");
+//   menu.classList.toggle("open");
+//   icon.classList.toggle("open");
+// }
+
+hamburger = document.querySelector(".hamburger-2");
+hamburger.onclick = function () {
+  navBar = document.querySelector(".nav-bar-2");
+  hamburger.classList.toggle("active");
+  navBar.classList.toggle("active");
+};
